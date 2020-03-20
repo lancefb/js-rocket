97d519c0b5644c2cbed24760a17c15aabff27021
Lance Barker <lancefb@gmail.com>
2020-03-20 00:19:34 -0700

    building rocket

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
-var numCars = 1;
+// change color function
+var doCoolStuff = function () {
+	var currentClassName = document.getElementById('cool').className;
+	// changing class name
+	if (currentClassName == 'cool') {
+		document.getElementById('cool').className = 'cool red'; }
+		else {
+			document.getElementById('cool').className = 'cool';
+		}
+	
+}
+
 var car = {
 	make: 'VW',
 	type: 'Polo',
